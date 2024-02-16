9a24c09e450f72486eba45540c8732fba27c5db2
deepankar666 <157325704+deepankar666@users.noreply.github.com>
2024-02-17 02:51:58 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request, jsonify
+import googletrans
+import speech_recognition
+import gtts
+import os
+import time
+import pygame
+
+app = Flask(__name__)
+
+def recognize_translate_play(text):
+    translator = googletrans.Translator()
+    translation = translator.translate(text, dest="hi")  # Translate to Hindi
+
+    # Define the path where the audio files will be saved
+    audio_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "audio")
+    os.makedirs(audio_dir, exist_ok=True)
+
+    # Generate a unique filename based on current timestamp
+    timestamp = int(time.time())
+    audio_file = os.path.join(audio_dir, f"tmp_audio_{timestamp}.mp3")
+
+    converted_audio = gtts.gTTS(translation.text, lang="hi")  # Convert to speech in Hindi
+    converted_audio.save(audio_file)
+    print("Saved audio file:", audio_file)
+
+    pygame.mixer.init()
+    pygame.mixer.music.load(audio_file)
+    pygame.mixer.music.play()
+
+    # Wait for the playback to finish
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)  # Adjust the frequency of checking
+
+    # Add a delay after playback ends to ensure the file is not deleted prematurely
+    time.sleep(5)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/translate', methods=['POST'])
+def translate():
+    if request.method == 'POST':
+        text = request.json['text']
+        recognize_translate_play(text)
+        return jsonify({'success': True})
+
+if __name__ == "__main__":
+    app.run(debug=True)
